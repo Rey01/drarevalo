17bea5fb9931b678fefb670a7d8d5cb8f3d4f5c4
Rey01 <thebestalpha2.3@gmail.com>
2019-11-14 00:46:12 -0600

    Las vistas consultan a la api

--- src/screens/Examenes.js
+++ src/screens/Examenes.js
@@ -1,21 +1,116 @@
 import React from 'react';
 import {
-  StyleSheet, ScrollView, Platform,
+  StyleSheet, ScrollView, Platform,AsyncStorage,ActivityIndicator,View
 } from 'react-native';
 
+import { LinearGradient as Gradient } from 'expo';
 // galio components
 import {
   Button, Block, Icon, Text, NavBar,
 } from 'galio-framework';
 import theme from '../theme';
 
+
 const BASE_SIZE = theme.SIZES.BASE;
 
 
-class Citas extends React.Component {
+const GRADIENT_BLUE = ['#6B84CA', '#8F44CE'];
+const GRADIENT_PINK = ['#D442F8', '#B645F5', '#9B40F8'];
+const COLOR_WHITE = theme.COLORS.WHITE;
+const COLOR_GREY = theme.COLORS.MUTED; // '#D8DDE1';
+
+
+class Examenes extends React.Component {
+  
+  state = {
+    cargando: true,
+    usuario: {
+      correoElectronico: "",
+      descripcion: "",
+      estado: "",
+      idAsignacionMembresia: "",
+      idExpediente: "",
+      nombreCompleto: "",
+      telemedicina: "",
+    },
+    cardsd:[]
+  }
+  retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('usuario');
+      if(value !== null) {
+        usuario = JSON.parse(value);
+        this.setState({usuario});
+        this.retrieveCitas();
+      }else{
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  retrieveCitas = async () => {
+    
+    var idAsignacionMembresia=this.state.usuario.idAsignacionMembresia;
+    var idExpediente=this.state.usuario.idExpediente;
+    var dataSend = new FormData();
+    dataSend.append('idAsignacionMembresia', idAsignacionMembresia);
+    dataSend.append('idExpediente', idExpediente);
+    return fetch(theme.COMPONENTS.URL_API+"GetExalemenesLaboratorio?idAsignacionMembresia="+idAsignacionMembresia+"&idExpediente="+idExpediente, {
+      method: 'POST',
+      body: dataSend,
+    })
+    .then((response) => response.json())
+    .then((resp) => {
+      console.log(resp.datos);
+      this.setState({cargando:false});
+      this.setState({cardsd:resp.datos});
+    }).catch((error) => {
+      console.error(error);
+    });
+  };
+  componentWillMount(){
+    this.retrieveData();
+  };
+
+  
+  renderCard = (props, index) => {
+    const gradientColors = index % 2 ? GRADIENT_BLUE : GRADIENT_PINK;
+
+    return (
+      <Block row center card shadow space="between" style={styles.card} key={props.idExamen}>
+        <Gradient
+          start={[0.45, 0.45]}
+          end={[0.90, 0.90]}
+          colors={gradientColors}
+          style={[styles.gradient, styles.left]}
+        >
+          <Icon
+            size={BASE_SIZE}
+            name="list-alt"
+            color={COLOR_WHITE}
+            family="font-awesome"
+          />
+        </Gradient>
+
+        <Block flex>
+          <Text size={BASE_SIZE * 1.125}>{props.nombreExamen}</Text>
+          <Text size={BASE_SIZE * 0.875} muted>{props.nombreArchivo}</Text>
+        </Block>
+        <Button style={styles.right}>
+          <Icon size={BASE_SIZE} name="arrow-right" family="font-awesome" color={COLOR_GREY} />
+        </Button>
+      </Block>
+    );
+  }
+
+  renderCards = () => this.state.cardsd.map((card, index) => this.renderCard(card, index))
+
+  
   renderHeader = () => (
     <NavBar
-      title="Citas"
+      title="Examenes"
       onLeftPress={() => this.props.navigation.openDrawer()}
       leftIconColor={theme.COLORS.MUTED}
       right={(
@@ -34,6 +129,13 @@ class Citas extends React.Component {
   
 
   render() {
+    if(this.state.cargando){
+      return (
+        <View style={styles.cargando}>
+          <ActivityIndicator size={theme.SIZES.BASE*2.5} color={theme.COLORS.ERROR} />
+        </View>
+      );
+    }else{
     return (
       <Block safe flex>
         {/* header */}
@@ -41,10 +143,11 @@ class Citas extends React.Component {
 
         {/* cards */}
         <ScrollView style={{ flex: 1 }}>
-          <Text>Examenes</Text>
+          {this.renderCards()}
         </ScrollView>
       </Block>
     );
+    }
   }
 }
 
@@ -56,7 +159,42 @@ const styles = StyleSheet.create({
   settings: {
     width: BASE_SIZE * 2,
     borderColor: 'transparent',
+  }, card: {
+    borderColor: 'transparent',
+    marginHorizontal: BASE_SIZE,
+    marginVertical: BASE_SIZE / 2,
+    padding: BASE_SIZE,
+    backgroundColor: COLOR_WHITE,
+    shadowOpacity: 0.40,
+  },
+  cargando:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  menu: {
+    width: BASE_SIZE * 2,
+    borderColor: 'transparent',
+  },
+  settings: {
+    width: BASE_SIZE * 2,
+    borderColor: 'transparent',
+  },
+  left: {
+    marginRight: BASE_SIZE,
+  },
+  right: {
+    width: BASE_SIZE * 2,
+    backgroundColor: 'transparent',
+    elevation: 0,
+  },
+  gradient: {
+    width: BASE_SIZE * 3.25,
+    height: BASE_SIZE * 3.25,
+    borderRadius: BASE_SIZE * 3.25,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
-export default Citas;
+export default Examenes;
